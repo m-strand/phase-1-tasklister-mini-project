aa0ec4be2640e9051d4f31d343eebeaf159f3203
Brooke Strand <brooke.strand007@gmail.com>
2022-01-30 14:34:18 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    manageToDo(e.target.tasks.value)
+    form.reset()
+  });
 });
+function manageToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  p.textContent = todo + " ";
+  btn.textContent = 'x';
+  btn.addEventListener("click", deleteToDo)
+  p.appendChild(btn);
+  document.getElementById('tasks').appendChild(p);
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove();
+}
